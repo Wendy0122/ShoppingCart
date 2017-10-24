67343bf2bb9a392f0c916f768bd9a9846db3c802
wufan <809247297@qq.com>
2017-10-24 16:29:54 +0800

    Initial commit

---
+++ app/src/main/java/com/bwei/shoppingcart/adapter/MyAdapter.java
@@ -0,0 +1,148 @@
+package com.bwei.shoppingcart.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bwei.shoppingcart.R;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1. 类的用途
+ * 2. @author forever
+ * 3. @date 2017/9/6 16:06
+ */
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private Context context;
+    private JSONArray data;
+    private Map<String, Boolean> map = new HashMap<>();
+
+    public MyAdapter(Context context, JSONArray data, OnCheckListener listener) {
+        this.context = context;
+        this.data = data;
+        setCheckData(false);
+        this.listener = listener;
+
+    }
+  //全选控制适配器item
+    private void setCheckData(boolean checkFlag) {
+        map.clear();
+        for (int i = 0; i < data.length(); i++) {
+            try {
+                JSONObject jsonObject = data.getJSONObject(i);
+                String id = jsonObject.getString("id");
+                map.put(id, checkFlag);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+ public void notifCheckData(boolean checkFlag){
+     setCheckData(checkFlag);
+     notifyDataSetChanged();
+ }
+    //创建view设置给ViewHolder
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, null);
+
+        MyViewHolder holder = new MyViewHolder(view);
+        return holder;
+    }
+
+    //绑定数据
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        try {
+            JSONObject jsonObject = data.getJSONObject(position);
+            String image_url = jsonObject.optString("image_url");
+            String title = jsonObject.getString("title");
+            final String id = jsonObject.getString("id");
+
+
+            holder.tv_title.setText(title);
+            Picasso.with(context).load(image_url).placeholder(R.mipmap.ic_launcher).into(holder.iv);
+
+            holder.cb.setChecked(map.get(id));
+
+            holder.cb.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //根据点击选中状态
+                    boolean checked = ((CheckBox) view).isChecked();
+                    map.put(id, checked);
+
+                    boolean isChecked = true;
+                    //遍历集合
+                    for (String key : map.keySet()) {
+                        Boolean value = map.get(key);
+                        if (!value) {
+                            isChecked = false;
+                            if (listener != null) {
+                                //取消全选
+                                listener.onCheck(isChecked);
+                            }
+                            return;
+                        }
+                    }
+                    if (isChecked) {
+                        //全选
+                        if (listener != null) {
+                            listener.onCheck(isChecked);
+                        }
+                    }
+                }
+            });
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //数据长度
+    @Override
+    public int getItemCount() {
+        return data.length();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView iv;
+        private TextView tv_title;
+        private CheckBox cb;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            iv = (ImageView) itemView.findViewById(R.id.iv);
+            tv_title = (TextView) itemView.findViewById(R.id.tv_title);
+            cb = (CheckBox) itemView.findViewById(R.id.cb);
+
+        }
+    }
+
+    private OnCheckListener listener;
+
+    public interface OnCheckListener {
+
+        void onCheck(boolean isCheck);
+    }
+
+    public void setOnCheckListener(OnCheckListener listener) {
+        this.listener = listener;
+    }
+}
